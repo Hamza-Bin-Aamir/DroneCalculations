a647b7d11c096ccffedd458843c0b62e51354c56
Hamza Bin Aamir <u2023219@giki.edu.pk>
2024-11-04 23:05:34 +0500

    Add a battery life calculator

---
+++ Battery Life/BatteryLifeAlt.py
@@ -0,0 +1,18 @@
+import atexit
+
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
+atexit.register(print, RESET)
+
+print(YELLOW + "*** MOTOR PARAMETERS ***")
+MotorCount = int(input("Please specify the number of motors: "))
+MotorAmp = float(input("Please specify the amperage of each motor (A): "))
+MotorLoad = float(input("Please specify the %age load at which the motor hovers: "))
+print("*** BATTERY PARAMETERS ***")
+BatteryCapacitymAh = int(input("Please enter the capacity of your battery (mAh): "))
+BatteryCapacityAh = BatteryCapacitymAh/1000
+
+print(GREEN + "YOUR BATTERY WILL LAST YOU " + YELLOW + f" {(BatteryCapacityAh/(MotorAmp*MotorCount*MotorLoad)):.2f} HOURS" + GREEN + ".")
